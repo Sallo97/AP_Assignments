29d0922c6e6e2ac11aa28f7437d6e0ce52f3f5af
Sal <salerno.salvatore1997@gmail.com>
2024-12-26 11:10:28 +0100

    Command Buttons now appear/disappear dynamically

---
+++ Assignment1_Matching_Pair_Game/src/main/java/sicily/sallo/assignment1_matching_pair_game/common_enums/GameState.java
@@ -0,0 +1,7 @@
+package sicily.sallo.assignment1_matching_pair_game.common_enums;
+
+public enum GameState {
+    MENU_SELECTION,
+    IN_GAME,
+    SCORE_BOARD,
+}
--- Assignment1_Matching_Pair_Game/src/main/java/sicily/sallo/assignment1_matching_pair_game/gui_components/CommandButtons.java
+++ Assignment1_Matching_Pair_Game/src/main/java/sicily/sallo/assignment1_matching_pair_game/gui_components/CommandButtons.java
@@ -2,13 +2,15 @@ package sicily.sallo.assignment1_matching_pair_game.gui_components;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
 /**
  * TODO Add better description
  * JPanel containing both the Shuffle Button and Exit Button
  */
-public class CommandButtons extends JPanel{
+public class CommandButtons extends JPanel implements ActionListener {
     // Properties
     public JButton shuffleButton = new JButton("SHUFFLE");
     public JButton exitButton = new JButton("EXIT");
@@ -26,10 +28,35 @@ public class CommandButtons extends JPanel{
         this.add(exitButton);
         this.add(highScoreButton);
 
+        shuffleButton.addActionListener(this);
+        exitButton.addActionListener(this);
+        highScoreButton.addActionListener(this);
+
         // Set action commands
         shuffleButton.setActionCommand("shuffle");
         exitButton.setActionCommand("exit");
         highScoreButton.setActionCommand("highScore");
 
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String command = e.getActionCommand();
+        switch (command) {
+            case "shuffle":
+                highScoreButton.setVisible(false);
+                break;
+
+            case "exit":
+                shuffleButton.setVisible(true);
+                highScoreButton.setVisible(true);
+                break;
+
+            case "highScore":
+                shuffleButton.setVisible(false);
+                highScoreButton.setVisible(false);
+                break;
+        }
     }
 }
